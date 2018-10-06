5a6593d9c3fbf020e42b8551b832a17e73f5d5e0
Kate Polley <kpolley@ithaca.edu>
2018-10-05 21:01:37 -0400

    create new artist

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, EqualTo
+from app.models import Artist
 
 class NewArtistForm(FlaskForm):
     name = StringField('Artist Name', validators=[DataRequired()])
     hometown = StringField('Hometown', validators=[DataRequired()])
     bio = TextAreaField('Biography', validators=[DataRequired()])
     submit = SubmitField('Create Artist')
+
+    def validate_name(self, name):
+        user = Artist.query.filter_by(name=name.data).first()
+        if user is not None:
+            raise ValidationError('This artist already exists.')
--- app/routes.py
+++ app/routes.py
@@ -9,14 +9,9 @@ def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/list_artists')
+@app.route('/artist')
 def list_artists():
-    artists = [
-        {'name': 'Hayley Kiyoko'},
-        {'name': 'Janelle Monae'},
-        {'name': 'King Princess'},
-        {'name': 'Tegan and Sara'}
-    ]
+    artists = Artist.query.all()
     return render_template('list.html', title='Artist List', artists=artists)
 
 
@@ -26,33 +21,32 @@ def new_artist():
 
     if form.validate_on_submit():
         flash('Artist Created: ' + form.name.data)
-        artist = {
-            'name': form.name.data,
-            'bio': form.bio.data,
-            'hometown': form.hometown.data,
-            'events': []
-        }
-        return render_template('artist.html', title='ArtistPage', artist=artist)
+        artist = Artist(name=form.name.data, hometown=form.hometown.data, bio=form.bio.data)
+        db.session.add(artist)
+        db.session.commit()
+
+        return redirect(url_for('list_artists'))
 
     return render_template('new.html', title='New Artist', form=form)
 
-@app.route('/artist_page')
-def artist_page():
-    artist = {
-        'name': 'Hayley Kiyoko',
-        'bio': 'Hayley Kiyoko is a 27-year-old American singer, songwriter, actor, dancer, and director.'
-               ' Her debut studio album Expectations was released on March 30, 2018.',
-        'hometown': 'Los Angeles, CA',
-        'events': ['October 23: 02 Academy Islington, London, UK',
-                   'October 24: Club Academy, Manchester, UK',
-                   'October 26: 02 Academy Islington, London, UK']
+@app.route('/artist/<name>')
+def artist_page(name):
+    artist = Artist.query.filter_by(name=name).first()
+    if artist is None:
+        flash('Artist ' + name + ' does not exist')
+        return redirect(url_for('list_artists'))
+    artist_info = {
+        'name': artist.name,
+        'bio': artist.bio,
+        'hometown': artist.hometown,
+        'events': artist.events,
     }
-    return render_template('artist.html', title='Artist Page', artist=artist)
+    return render_template('artist.html', title=artist.name, artist=artist_info)
 
 
 @app.route('/reset_db')
 def reset_db():
-    flash("Resetting Database: deleting old data and repopulating with dummy data")
+    flash('Resetting Database: deleting old data and repopulating with dummy data')
     # clear all data from all tables
     meta = db.metadata
     for table in reversed(meta.sorted_tables):
@@ -62,17 +56,20 @@ def reset_db():
 
     # populate tables with dummy data
     artists = [
-        Artist(name="Hayley Kiyoko", hometown='Los Angeles, CA', bio=''),
-        Artist(name="King Princess", hometown='Brooklyn, CA', bio=''),
-        Artist(name="Janelle Monae", hometown='Atlanta, GA', bio=''),
-        Artist(name="Kehlani", hometown='Oakland, CA', bio=''),
-        Artist(name="Mary Lambert", hometown='Seattle, WA', bio='')
+        Artist(name='Hayley Kiyoko', hometown='Los Angeles, CA', bio='Singer. Songwriter. Actor. Director. Lesbian Jesus.'),
+        Artist(name='King Princess', hometown='Brooklyn, CA', bio='This singer-songwriter is known for her personal narrative lyrics'),
+        Artist(name='Janelle Monae', hometown='Atlanta, GA', bio='An accomplished musician and actor with a captivating voice'),
+        Artist(name='Kehlani', hometown='Oakland, CA', bio='R&B and hip-hop that anyone can get into'),
+        Artist(name='The Indigo Girls', hometown='Atlanta, GA', bio='The iconic, award-winning lesbian folk rock duo'),
+        Artist(name='Amythyst Kiah', hometown='Johnson City, TN', bio='A Southern Gothic songster with a hypnotic sound'),
     ]
     db.session.add_all(artists)
 
     venues = [
-        Venue(name='02 Academy Islington', city='London', country='England'),
-        Venue(name='Forum Theatre', city='Melbourne', country='Australia')
+        Venue(name='02 Academy Islington', city='London', country='UK'),
+        Venue(name='Forum Theatre', city='Melbourne', country='Australia'),
+        Venue(name='The Ark', city='Ann Arbor', country='USA'),
+        Venue(name='City Park', city='New Orleans, LA', country='USA')
     ]
     db.session.add_all(venues)
 
@@ -80,6 +77,9 @@ def reset_db():
         Event(name='Expectations Tour 1', date='October 23, 2018', venue=venues[0]),
         Event(name='Expectations Tour 2', date='October 26, 2018', venue=venues[0]),
         Event(name='King Princess Tour', date='November 2, 2018', venue=venues[1]),
+        Event(name='King Princess For Real', date='December 17, 2018', venue=venues[2]),
+        Event(name='Amy Ray & Her Band', date='November 14, 2018', venue=venues[2]),
+        Event(name='Voodoo Music + Arts Experience', date='October 26, 2018', venue=venues[3]),
     ]
     db.session.add_all(events)
 
@@ -88,6 +88,10 @@ def reset_db():
         ArtistToEvent(artist=artists[0], event=events[1], headliner=True),
         ArtistToEvent(artist=artists[3], event=events[1], headliner=False),
         ArtistToEvent(artist=artists[1], event=events[2], headliner=True),
+        ArtistToEvent(artist=artists[1], event=events[3], headliner=True),
+        ArtistToEvent(artist=artists[4], event=events[4], headliner=True),
+        ArtistToEvent(artist=artists[5], event=events[4], headliner=False),
+        ArtistToEvent(artist=artists[2], event=events[5], headliner=False),
     ]
     db.session.add_all(associations)
 
