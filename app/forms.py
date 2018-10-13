2fdc485c7fe0d9ebce7729f02b4c8148e9d51006
Kate Polley <kpolley@ithaca.edu>
2018-10-13 19:41:46 -0400

    users and login

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
-from app.models import Artist
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
+from app.models import Artist, User
 
 class NewArtistForm(FlaskForm):
     name = StringField('Artist Name', validators=[DataRequired()])
@@ -13,3 +13,27 @@ class NewArtistForm(FlaskForm):
         user = Artist.query.filter_by(name=name.data).first()
         if user is not None:
             raise ValidationError('This artist already exists.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
